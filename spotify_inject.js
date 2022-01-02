18b27788a71436edea1d1dd5936ec7e4c8274791
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-01-02 18:27:49 +0000

    Project on hold for now - backing up local changes

--- spotify_inject.js
+++ spotify_inject.js
@@ -1,9 +1,8 @@
 var base = document.createElement; /* A backup reference to the browser's original document.createElement */
 
 var MediaElements = [];
-var Iframes = [];
 
-var Doc;
+timeout();
 
 /* Replacing the DOM's original reference to the browser's createElement function */
 document.createElement = function(message) {
@@ -11,11 +10,28 @@ document.createElement = function(message) {
     
     if(message == 'video' || message == 'audio') {
         MediaElements.push(element); /* Arrays hold references not copies by default in javascript. */
-    }
-
-    if(message == 'iframe') {
-        Iframes.push(element);
+        console.log(element);
     }
 
     return element
 };
+
+function changeSpeed(val) {
+    for(var i = 0; i < MediaElements.length; i++){ /* change speed for all elements found (i havent seen this be more than 1 but you never know) */
+        MediaElements[i].playbackRate = val; /* set the playback rate here */
+        if(val != 64){ /* update the lastSpeed if the speed actually changed */
+            lastSpeed = val;
+        }
+    }
+}
+
+function timeout() { /* This function is called by itself over and over */
+    setTimeout(function () { /* setTimeout is a delayed call(500 milliseconds) to the code below */
+        try {
+            changeSpeed(2); /* this is in a try/catch because if an error happens timeout wouldnt be called again. */
+        }catch{
+            
+        }
+        timeout(); /* call timeout again which starts the loop and eventually it will come back here */
+    }, 500); /* 500ms */
+}
